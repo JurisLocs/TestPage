7fb6c80baa4d8275180c636672b4f155f770592f
JurisLocs <78651682+JurisLocs@users.noreply.github.com>
2021-02-07 12:53:38 +0200

    Update effects.js

--- effects.js
+++ effects.js
@@ -1,19 +1,52 @@
 $(document).ready(function(){
+    var id = (".active".id)
+    $(".bankname").text(id) 
     $('.carousel').carousel({
         interval: false,
       });
     
     $(".bankas").click(function(){
         $(this).siblings('.bankas').removeClass('activeBank');
-        $(this).addClass("activeBank");
-        if($('#banka').hasClass('activeBank')){
-            ('#myCarousel').removeClass('.inactive');
-            ('#myCarousel2').addClass('.inactive');
-        } else {
-            ('#myCarousel2').removeClass('.inactive');
-            ('#myCarousel').addClass('inactive');
-        }
+        $(this).addClass("activeBank");    
 
 
+    })
+    $("#citadele").click(function(){       
+        $(".bankname").text("Citadele") 
+       
+    })
+    $("#citadele2").click(function(){       
+        $(".bankname").text("Citadele") 
+       
+    })
+    $("#swedbank").click(function(){       
+        $(".bankname").text("Swedbank") 
+       
+    })
+    $("#seb").click(function(){       
+        $(".bankname").text("SEB") 
+       
+    })
+    $("#luminor").click(function(){       
+        $(".bankname").text("Luminor") 
+       
+    })
+    $("#blueorange").click(function(){       
+        $(".bankname").text("BlueOrange") 
+       
+    })
+
+    $("#lombards").click(function(){
+        $("#myCarousel2").removeClass("inactive")
+        $("#myCarousel").addClass("inactive")
+        
+        
+
+    })
+
+    $("#banka").click(function(){
+        $("#myCarousel").removeClass("inactive")
+        $("#myCarousel2").addClass("inactive")
+        
     })
 })
